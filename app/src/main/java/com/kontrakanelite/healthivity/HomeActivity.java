ca69abd4bc62cf558e82c4e93fde0550b88eaf28
miftahfarhan13 <miftahfarhan20@gmail.com>
2019-04-30 09:59:31 +0700

    front end home login regist profile push commit

--- app/src/main/java/com/kontrakanelite/healthivity/HomeActivity.java
+++ app/src/main/java/com/kontrakanelite/healthivity/HomeActivity.java
@@ -1,13 +1,32 @@
 package com.kontrakanelite.healthivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+    private RecyclerView rvKategori;
+    private ImageView ivSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        rvKategori = (RecyclerView) findViewById(R.id.rvKategoriHome);
+        rvKategori.setLayoutManager(new GridLayoutManager(this,3));
+
+        ivSetting = (ImageView) findViewById(R.id.ivSettingHome);
+        ivSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
